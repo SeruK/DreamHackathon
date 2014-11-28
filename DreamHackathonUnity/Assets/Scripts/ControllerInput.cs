4b1d02e3d85d8f476b3dbf03c1d2324b45740245
SebastianStrand <Sebastian@ImageForm.se>
2014-11-28 23:01:01 +0100

    Initial scene; Controller support; Temp game code

---
+++ DreamHackathonUnity/Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ControllerInput
+{
+	// Might not be correct with this ver of Tattie Bogle
+	public enum Button
+	{
+		A = 16,
+		B = 17,
+		X = 18,
+		Y = 19,
+		Start = 9,
+		Back = 10,
+		BumperLeft = 14,
+		BumperRight = 15,
+		DpadUp = 5,
+		DpadDown = 6,
+		DpadLeft = 7,
+		DpadRight = 8
+	}
+
+	public enum Range
+	{
+//		LeftTrigger,
+//		RightTrigger,
+		LeftStick,
+		RightStick,
+//		Dpad
+	}
+
+	public enum Axis
+	{
+		X,
+		Y
+	}
+
+	public static bool GetButton(uint in_controller, Button in_button)
+	{
+		return Input.GetKey(GetKeyCode(in_controller, in_button));
+	}
+
+	public static bool GetButtonDown(uint in_controller, Button in_button)
+	{
+		return Input.GetKeyDown(GetKeyCode(in_controller, in_button));
+	}
+
+	public static bool GetButtonUp(uint in_controller, Button in_button)
+	{
+		return Input.GetKeyUp(GetKeyCode(in_controller, in_button));
+	}
+
+	public static float GetAxis(uint in_controller, Range in_range, Axis in_axis)
+	{
+		return Input.GetAxis(GetRangeString(in_controller, in_range, in_axis));
+	}
+
+	public static Vector2 GetRange(uint in_controller, Range in_range)
+	{
+		return new Vector2(GetAxis(in_controller, in_range, Axis.X),
+		                   GetAxis(in_controller, in_range, Axis.Y));
+	}
+
+	private static KeyCode GetKeyCode(uint in_controller, Button in_button)
+	{
+		string str = GetControllerName(in_controller) + "Button" + (int)in_button;
+		return (KeyCode)System.Enum.Parse(typeof(KeyCode), str);
+	}
+
+	private static string GetControllerName(uint in_controller)
+	{
+		return "Joystick" + (in_controller + 1);
+	}
+
+	public static string GetButtonName(Button in_button)
+	{
+		return System.Enum.GetName(typeof(Button), in_button);
+	}
+
+	public static string GetRangeName(Range in_range)
+	{
+		return System.Enum.GetName(typeof(Range), in_range);
+	}
+
+	private static string GetRangeString(uint in_controller, Range in_range, Axis in_axis)
+	{
+		string axis = in_axis == Axis.X ? "XAxis" : "YAxis";
+
+		return GetInternalRangeName(in_range) + "_" + axis + "_" + (in_controller + 1);
+	}
+
+	private static string GetInternalRangeName(Range in_range)
+	{
+		switch (in_range)
+		{
+			case Range.LeftStick:
+			{
+				return "L";
+			}
+			case Range.RightStick:
+			{
+				return "R";
+			}
+//			case Range.LeftTrigger:
+//			{
+//				return "TriggersL";
+//			}
+//			case Range.RightTrigger:
+//			{
+//				return "TriggersR";
+//			}
+//			case Range.Dpad:
+//			{
+//				return "DPad";
+//			}
+
+			default: break;
+		}
+
+		throw new System.Exception("Shouldn't come here!");
+	}
+}
--- DreamHackathonUnity/Assets/Scripts/Game.cs
+++ DreamHackathonUnity/Assets/Scripts/Game.cs
@@ -3,4 +3,51 @@ using System.Collections;
 
 public class Game : MonoBehaviour
 {
+	void Update()
+	{
+
+	}
+
+	public delegate void DrawDelegate();
+
+	void Indent(int in_amount, DrawDelegate in_draw)
+	{
+		GUILayout.BeginHorizontal();
+		GUILayout.Space(in_amount * 10.0f);
+		if (in_draw != null) in_draw();
+		GUILayout.EndHorizontal();
+	}
+
+	void OnGUI()
+	{
+		for (uint controller = 1; controller < 2; ++controller)
+		{
+			GUILayout.Label("Controller " + (controller + 1));
+
+			foreach (ControllerInput.Range range in System.Enum.GetValues(typeof(ControllerInput.Range)))
+			{
+				Vector2 val = ControllerInput.GetRange(controller, range);
+				GUILayout.Label(ControllerInput.GetRangeName(range) + ": " + val);
+           	}
+			foreach (ControllerInput.Button button in System.Enum.GetValues(typeof(ControllerInput.Button)))
+			{
+				string state = "";
+				if (ControllerInput.GetButtonDown(controller, button))
+				{
+					state += " Down";
+				}
+				if (ControllerInput.GetButton(controller, button))
+				{
+					state += " Pressed";
+				}
+				if (ControllerInput.GetButtonUp(controller, button))
+				{
+					state += " Up";
+				}
+				Indent(1, () => (GUILayout.Label(ControllerInput.GetButtonName(button) + ":" + state)));
+			}
+
+        	GUILayout.Space(10.0f);
+		}
+	}
 }
