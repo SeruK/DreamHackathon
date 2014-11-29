8ef34b9433a2951b0db5e6293b417b61ec2e09b7
SebastianStrand <Sebastian@ImageForm.se>
2014-11-29 15:53:04 +0100

    Trees

---
+++ DreamHackathonUnity/Assets/Editor/RandomWindow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class RandomWindow : EditorWindow
+{
+	[MenuItem("Window/Random")]
+	static void Init()
+	{
+		EditorWindow.GetWindow(typeof(RandomWindow));
+	}
+
+	bool randomizeRot;
+	float minScale;
+	float maxScale;
+
+	void Randomize()
+	{
+		foreach (var go in Selection.gameObjects)
+		{
+			if (randomizeRot)
+			{
+				go.transform.Rotate(Vector3.up, Random.Range(0.0f, 360.0f));
+			}
+
+			go.transform.localScale = new Vector3(1, 1, 1) * Random.Range(minScale, maxScale);
+		}
+	}
+
+	void OnGUI()
+	{
+		GUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField(minScale.ToString());
+		EditorGUILayout.MinMaxSlider(ref minScale, ref maxScale, 0.1f, 3.0f);
+		EditorGUILayout.LabelField(maxScale.ToString());
+		GUILayout.EndHorizontal();
+		randomizeRot = EditorGUILayout.Toggle("Randomize rotation", randomizeRot);
+		if (GUILayout.Button("Randomize"))
+		{
+			Randomize();
+		}
+	}
+}
