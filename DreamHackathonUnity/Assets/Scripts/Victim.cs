62ba6457a245519fe4c0097d2f12004ddf89d8eb
SebastianStrand <Sebastian@ImageForm.se>
2014-11-29 14:27:12 +0100

    Hunter killing

--- DreamHackathonUnity/Assets/Scripts/Victim.cs
+++ DreamHackathonUnity/Assets/Scripts/Victim.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class Victim : MonoBehaviour
 {
+	public float LookSphereRadius = 0.5f;
+
+	public Transform RaycastOrigin; 
+	public Transform Model;
+	public Transform CameraRoot;
+
+#if UNITY_EDITOR
+	public float DebugGizmoDistance = 5.0f;
+#endif
+
 	void OnNetworkInstantiate(NetworkMessageInfo in_info)
 	{
 		if (Network.isClient)
@@ -17,4 +27,42 @@ public class Victim : MonoBehaviour
 		var game = GameObject.FindGameObjectWithTag("GameController").GetComponent<Game>();
 		game.OnVictimSpawned(this);
 	}
+
+	Ray GetRay()
+	{
+		return new Ray(RaycastOrigin.position, RaycastOrigin.TransformDirection(Vector3.forward));
+	}
+
+	void Update()
+	{
+		Model.rotation = Quaternion.Euler(0, CameraRoot.rotation.eulerAngles.y, 0.0f);
+		var ray = GetRay(); 
+
+		Debug.DrawLine(ray.origin, ray.origin + ray.direction * 10000.0f);
+//		Debug.DrawRay(ray.origin, ray.direction);
+
+		if (!Network.isServer) return;
+
+		// TODO: Add props layer here
+		int layers = (1 << 9) | (1 << 10) | (1 << 11) | (1 << 12);
+
+		RaycastHit hitInfo;
+
+		if (Physics.SphereCast(ray, LookSphereRadius, out hitInfo, 100000.0f, layers))
+		{
+			Debug.Log("Hit! " + hitInfo.collider);
+			hitInfo.collider.BroadcastMessage("WasHit", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+#if UNITY_EDITOR
+	void OnDrawGizmosSelected()
+	{
+		Ray ray = GetRay();
+		var start = ray.origin;
+		var end = ray.origin + ray.direction * DebugGizmoDistance;
+		Gizmos.DrawLine(start, end);
+		Gizmos.DrawWireSphere(end, LookSphereRadius);
+	}
+#endif
 }
